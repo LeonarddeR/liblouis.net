54c3aa652fab0b62195b49e7d2540db22273a992
Leonard de Ruijter <L.de.Ruijter@sevenp.nl>
2020-06-12 08:19:51 +0200

    Add first files

---
+++ liblouis.Net.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace liblouis.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ liblouis.Net/Exceptions/DisplayException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace liblouis.Exceptions
+{
+    class DisplayException: Exception
+    {
+    }
+}
